5b16e2abec046a423492fab4959509ee5c5b3822
VivekvivekP <vivekp8681@gmail.com>
2022-01-01 09:48:21 +0530

    Username functionality

--- src/com/test/Instagram.java
+++ src/com/test/Instagram.java
@@ -2,11 +2,16 @@ package com.test;
 
 public class Instagram {
 
-	public static void userName() {
-		System.out.println("username");
+	public static void Username() {
+		System.out.println("password");
+	}
+	
+	public static void firstName() {
+		System.out.println("firstName");
 	}
 	
 	public static void main(String[] args) {
-		userName();
+		Username();
+		firstName();
 	}
 }
